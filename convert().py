551a66eb6f99df5bbe3cc5ddb50f9c374f6bbaeb
jc321013 <jmarcolongo1@gmail.com>
2015-10-02 22:29:25 +1000

    prints output

--- convert().py
+++ convert().py
@@ -8,6 +8,22 @@ x = urllib.request.urlopen("https://www.google.com/finance/converter?a=1&from=AU
 print(x.read())
 
 url = 'https://www.google.com/finance/converter?a=1&from=AUD&to=JPY&meta=ei%3DO3YOVpGGGMaE0gS7jIuQAQ'
+values = {'84.5890': 'aud,yen',
+          'submit': 'convert'}
+
+data = urllib.parse.urlencode(values)
+data = data.encode('utf-8')
+req = urllib.request.Request(url,data)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+
+print(respData)
+
+x = urllib.request.urlopen("https://www.google.com/finance/converter?a=84.5890&from=JPY&to=AUD")
+
+print(x.read())
+
+url = 'https://www.google.com/finance/converter?a=84.5890&from=JPY&to=AUD&meta=ei%3DB3gOVuHIB8SN0ATx0buoAQ'
 values = {'1': 'aud,yen',
           'submit': 'convert'}
 
